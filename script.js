4355ff7650005a42876b60ca5743ab91c4a0eaba
orthodox2000 <orthodox2000>
2025-12-29 14:14:59 +0530

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+const data=[
+    ["Mediacare"," NextJS based project for Pixel Cup"],
+    ["Patient Portal","Full Stack Web App For Hospitals"],
+    ["Mediacare"," NextJS based project for Pixel Cup"],
+    ["Patient Portal","Full Stack Web App For Hospitals"],
+    ["Mediacare"," NextJS based project for Pixel Cup"],
+    ["Patient Portal","Full Stack Web App For Hospitals"]
+]
+ 
